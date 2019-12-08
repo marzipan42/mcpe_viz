d6c11b22483869327c7ef63141761a572b28acf9
Asa Packer <asa.packer@comcast.net>
2019-12-08 15:56:49 -0800

    add comment about version of windows 10 needed for tar to be available

--- _01_mygetdeps.ps1
+++ _01_mygetdeps.ps1
@@ -1,8 +1,11 @@
+git clone https://github.com/marzipan42/leveldb-mcpe.git
+git clone https://github.com/marzipan42/libnbtplusplus.git
+
 curl "http://xmlsoft.org/sources/libxml2-2.9.10.tar.gz" -OutFile libxml2-2.9.10.tar.gz
 curl "https://managedway.dl.sourceforge.net/project/libpng/libpng16/1.6.37/lpng1637.zip" -OutFile lpng1637.zip
 curl "https://www.zlib.net/zlib1211.zip" -OutFile zlib1211.zip
-git clone https://github.com/marzipan42/leveldb-mcpe.git
-git clone https://github.com/marzipan42/libnbtplusplus.git
+
+# native tar added to powershell in windows 10 1803
 tar zxvf libxml2-2.9.10.tar.gz
 expand-archive lpng1637.zip .
 expand-archive zlib1211.zip .
