11afdec057a292c28d0ce7c723b230bc90b7a7c6
Asa Packer <asa.packer@comcast.net>
2019-12-08 10:49:29 -0800

    added script to download dependencies

---
+++ _01_mygetdeps.ps1
@@ -0,0 +1,8 @@
+curl "http://xmlsoft.org/sources/libxml2-2.9.10.tar.gz" -OutFile libxml2-2.9.10.tar.gz
+curl "https://managedway.dl.sourceforge.net/project/libpng/libpng16/1.6.37/lpng1637.zip" -OutFile lpng1637.zip
+curl "https://www.zlib.net/zlib1211.zip" -OutFile zlib1211.zip
+git clone https://github.com/marzipan42/leveldb-mcpe.git
+git clone https://github.com/marzipan42/libnbtplusplus.git
+tar zxvf libxml2-2.9.10.tar.gz
+expand-archive lpng1637.zip .
+expand-archive zlib1211.zip .
